5026905a36e507cd504885b6a9bd0170bfb065f5
LinoCondori <condori.lino.f@gmail.com>
2026-02-27 17:36:25 -0300

    Se agregaron dos plots, de 1 hora y 24horas.

--- instrument.py
+++ instrument.py
@@ -2,13 +2,17 @@
 import threading
 import random
 import time
+import os
+import pandas as pd
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
+
 
 class Instrument:
     def __init__(self, name, port, instrument_id):
         self.name = name
         self.port = port
         self.instrument_id = instrument_id
-        self.enable = False
         self.value = None
         self.running = False
 
@@ -29,8 +33,35 @@ class Instrument:
 
     def read_value(self):
         # Simulación de lectura
-        return round(random.uniform(0, 100), 2)
+        data1= round(random.uniform(0, 100), 2)
+        self.save_data(data1, round(random.uniform(0, 100), 2))
+        return data1
 
-        #self.last_value = round(random.uniform(0, 100), 2)
-        #self.last_update = time.strftime("%Y-%m-%d %H:%M:%S")
-        #return self.last_value
+
+    def save_data(self, data1, data2):
+        now = datetime.now()
+
+        year = now.strftime("%Y")
+        date_str = now.strftime("%Y-%m-%d")
+        timestamp = now.strftime("%Y-%m-%d %H:%M:%S")
+
+        base_path = os.path.join("data", self.name, "crudo", year)
+        os.makedirs(base_path, exist_ok=True)
+
+        file_path = os.path.join(base_path, f"{date_str}.csv")
+
+        file_exists = os.path.isfile(file_path)
+
+        with open(file_path, "a") as f:
+            if not file_exists:
+                f.write("FechaHora,dato1,dato2\n")
+
+            f.write(f"{timestamp},{data1},{data2}\n")
+
+    def load_data_from_file(self, file):
+
+        try:
+            df = pd.read_csv(file, parse_dates=["FechaHora"])
+        except:
+            df = pd.DataFrame()
+        return df
--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 # gui.py
 import tkinter as tk
+from tkinter import filedialog
+
+import pandas as pd
+
 from manager import InstrumentManager
+import json
+from datetime import datetime, timedelta
+import os
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.figure import Figure
 
 class App:
     def __init__(self, root):
@@ -12,21 +21,21 @@ class App:
         menubar = tk.Menu(self.root)
 
         menu_archivo = tk.Menu(menubar, tearoff=0)
-        menu_archivo.add_command(label="Abrir configuración")
-        menu_archivo.add_command(label="Guardar configuración")
+        menu_archivo.add_command(label="Guardar", command=self.save_config)
+        menu_archivo.add_command(label="Guardar como...", command=self.save_config_as)
+        menu_archivo.add_command(label="Abrir", command=self.load_config)
         menu_archivo.add_separator()
-        menu_archivo.add_command(label="Salir", command=root.quit)
+        menu_archivo.add_command(label="Salir", command=self.root.quit)
 
         menubar.add_cascade(label="Archivo", menu=menu_archivo)
 
         self.root.config(menu=menubar)
 
-
         self.manager = InstrumentManager()
 
         # Frame superior
         top_frame = tk.Frame(root)
-        top_frame.pack(pady=10)
+        top_frame.pack(side="top", anchor="nw", pady=10)
         new_inst_frame = tk.Frame(top_frame)
         new_inst_frame.pack(side=tk.LEFT, pady=10)
         self.entry_name = tk.Entry(new_inst_frame)
@@ -43,11 +52,28 @@ class App:
         self.instruments_frame = tk.Frame(top_frame)
         self.instruments_frame.pack(pady=10, side="left")
 
-        self.labels = {}
         self.inst_frame = {}
 
+        try:
+            self.load_config("last_config.json")
+        except Exception as e:
+            print("Error cargando config:", e)
+
+        # Frame para gráficos
+        self.graph_frame = tk.Frame(root)
+        self.graph_frame.pack(fill="both", expand=True)
+
+        self.figure = Figure(figsize=(8, 6), dpi=100)
+
+        self.ax_hour = self.figure.add_subplot(121)  # izq
+        self.ax_24h = self.figure.add_subplot(122)  # der
+
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.graph_frame)
+        self.canvas.get_tk_widget().pack(side="bottom", fill="both", expand=True)
 
         self.update_loop()
+        self.update_graph()
+
 
     def add_instrument(self):
         name = self.entry_name.get()
@@ -58,47 +84,50 @@ class App:
             if instrument is None:
                 print("Ya existe un instrumento con ese nombre")
                 return
-            frame = tk.Frame(self.instruments_frame)
-            frame.pack(side="left")
+            self.create_instrument_ui(instrument, enabled=True)
+
 
             self.entry_name.delete(0, tk.END)
             self.entry_port.delete(0, tk.END)
             self.entry_instrument_id.delete(0, tk.END)
-            btn_delete = tk.Button(
-                frame,
-                text="X",
-                fg="red",
-                command=lambda inst=instrument: self.remove_instrument(inst)
-            )
-            btn_delete.pack(anchor="ne",side="right")
-            label_name = tk.Label(frame, text=name)
-            label_name.pack()
-            label_port = tk.Label(frame, text=port)
-            label_port.pack()
-            label_instrument_id = tk.Label(frame, text=instrument_id)
-            label_instrument_id.pack()
-            label_value = tk.Label(frame, text=f"--")
-            label_value.pack()
-            var_enabled = tk.BooleanVar(value=True)
-            check = tk.Checkbutton(
-                frame,
-                text="Habilitado",
-                variable=var_enabled,
-                command=lambda inst=instrument, var=var_enabled: self.toggle_instrument(inst, var),
-            )
-            check.pack()
+    def create_instrument_ui(self, instrument, enabled=True):
 
-            self.inst_frame[instrument] = {
-                "frame": frame,
-                "label_value": label_value,
-                "enable":var_enabled,
-            }
+        frame = tk.Frame(self.instruments_frame)
+        frame.pack(side="left")
 
+        btn_delete = tk.Button(
+            frame,
+            text="X",
+            fg="red",
+            command=lambda inst=instrument: self.remove_instrument(inst)
+        )
+        btn_delete.pack(anchor="ne", side="right")
+
+        tk.Label(frame, text=instrument.name).pack()
+        tk.Label(frame, text=instrument.port).pack()
+        tk.Label(frame, text=instrument.instrument_id).pack()
+
+        label_value = tk.Label(frame, text="--")
+        label_value.pack()
+
+        var_enabled = tk.BooleanVar(value=enabled)
+
+        check = tk.Checkbutton(
+            frame,
+            text="Habilitado",
+            variable=var_enabled,
+            command=lambda inst=instrument, var=var_enabled: self.toggle_instrument(inst, var),
+        )
+        check.pack()
+
+        self.inst_frame[instrument] = {
+            "frame": frame,
+            "label_value": label_value,
+            "enable": var_enabled,
+        }
+
+        if enabled:
             instrument.start()
-            #label = tk.Label(self.instruments_frame, text=f"{name}: --")
-            #label.pack(side="left")
-            #self.labels[name] = label
-            #self.entry_name.delete(0, tk.END)
 
     def update_loop(self):
         for instrument in self.manager.get_instruments():
@@ -127,6 +156,114 @@ class App:
             self.inst_frame[instrument]["frame"].destroy()
 
             del self.inst_frame[instrument]
+
+    # esta funcion es para obtener la configuracion actual
+    def get_config_data(self):
+        return {
+            "instrumentos": [
+                {
+                    "name": inst.name,
+                    "port": inst.port,
+                    "instrument_id": inst.instrument_id,
+                    "enabled": inst.running
+                }
+                for inst in self.manager.get_instruments()
+            ]
+        }
+
+
+
+    def save_config(self, filename="last_config.json"):
+        data = self.get_config_data()
+        with open(filename, "w") as f:
+            json.dump(data, f, indent=4)
+
+
+
+    def save_config_as(self):
+        file = filedialog.asksaveasfilename(
+            defaultextension=".json",
+            filetypes=[("JSON files", "*.json")]
+        )
+
+        if file:
+            self.save_config(file)
+
+    def load_config(self, filename=None):
+
+        if not filename:
+            filename = filedialog.askopenfilename(
+                filetypes=[("JSON files", "*.json")]
+            )
+
+        if not filename:
+            return
+
+        with open(filename, "r") as f:
+            data = json.load(f)
+
+        #  Primero limpiar todo
+        for inst in list(self.manager.get_instruments()):
+            self.remove_instrument(inst)
+
+        #  Crear instrumentos nuevos
+        for item in data["instrumentos"]:
+            instrument = self.manager.add_instrument(
+                item["name"],
+                item["port"],
+                item["instrument_id"]
+            )
+
+            enabled = item.get("enabled", False)
+            self.create_instrument_ui(instrument, enabled=enabled)
+
+
+    def create_path(self, fechaHora, instrument):
+        year = fechaHora.strftime("%Y")
+        date_str = fechaHora.strftime("%Y-%m-%d")
+
+        base_path = os.path.join("data", instrument, "crudo", year)
+        return os.path.join(base_path, f"{date_str}.csv")
+
+    def update_graph(self):
+        now = datetime.now()
+        yesterday = now - timedelta(hours=24)
+
+
+        self.ax_hour.clear()
+        self.ax_24h.clear()
+
+        for instrument in self.manager.get_instruments():
+            file_path_now = self.create_path(now, instrument.name)
+            file_path_yesterday = self.create_path(yesterday, instrument.name)
+            df_full = pd.concat([instrument.load_data_from_file(file_path_now) , instrument.load_data_from_file(file_path_yesterday)], axis=0)
+
+            filter_1H = df_full["FechaHora"] >= pd.to_datetime(now) - pd.to_timedelta(1, "hour")
+            print(type(filter_1H))
+
+            df = df_full.loc[filter_1H]
+
+            if df is not None and not df.empty:
+                self.ax_hour.plot(df["FechaHora"], df["dato1"], label=instrument.name)
+
+            filter_24H = df_full["FechaHora"] >= pd.to_datetime(now) - pd.to_timedelta(24, "hour")
+            df = df_full.loc[filter_24H]
+            if df is not None and not df.empty:
+                self.ax_24h.plot(df["FechaHora"], df["dato1"], label=instrument.name)
+
+        self.ax_hour.legend()
+        self.ax_hour.set_title("Última hora")
+        self.ax_hour.tick_params(axis='x', rotation=45)
+
+        self.ax_24h.legend()
+        self.ax_24h.set_title("Última 24 Horas")
+        self.ax_24h.tick_params(axis='x', rotation=45)
+
+
+        self.canvas.draw()
+
+        self.root.after(10000, self.update_graph)  # cada 10 segundos
+
 
 if __name__ == "__main__":
     root = tk.Tk()
