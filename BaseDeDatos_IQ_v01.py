c234b2d28db526d869a63c5e226be566363ad6a4
LinoCondori <condori.lino.f@gmail.com>
2026-03-16 14:32:48 -0300

    se resolvio problemas de sintaxis

--- BaseDeDatos_IQ_v01.py
+++ BaseDeDatos_IQ_v01.py
@@ -9,16 +9,16 @@ from time import time
 
 def buscarEnBaseDeDatos(engine, Tabla, inicio, fin):
     try:
-        consulta = 'Select * from ´' + Tabla + '´ where ´time_stamp´ >= \'' + inicio._repr_base + '\' and ´time_stamp´ < \'' + fin._repr_base + '\' order by ´time_stamp´'
+        consulta = "Select * from '" + Tabla + "' where 'time_stamp' >= \'" + inicio._repr_base + "\' and 'time_stamp' < \'" + fin._repr_base + "\' order by 'time_stamp'"
         print(consulta)
         df_aux = pd.read_sql_query(consulta, con=engine)
-        df_aux.DateTime = pd.to_datetime(df_aux.DateTime)
-        df_aux.set_index(['time_stamp'], inplace=True)
+        df_aux.DateTime = pd.to_datetime(df_aux.time_stamp)
+        df_aux.set_index(['DateTime'], inplace=True)
         df_aux['DateTime'] = df_aux.index
         df_aux.index.name = 'DateTime'
-        # df_aux.rename({'TambC': 'TempA', 'Hr': 'HR', 'PrhPa': 'Pres', 'VdGrad': 'Dir'}, axis=1, inplace=True)
     except:
         print("No se pudo Obtener Datos de " + inicio._repr_base)
         df_aux = pd.DataFrame()
     # print(df_aux)
     return df_aux
+
